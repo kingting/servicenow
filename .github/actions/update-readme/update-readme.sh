841f40df4d00b952e055a53c3d6f3ab4a7bf6bb9
King Ting <king@ozhut.com>
2024-06-06 14:49:21 +1000

    More debugging info

--- .github/actions/update-readme/update-readme.sh
+++ .github/actions/update-readme/update-readme.sh
@@ -23,9 +23,17 @@ read_file_content() {
 SERVICENOW_JS=$(cat servicenow.js)
 #DEPLOY_SCRIPT_CONTENT=$(cat deploy.sh)
 #CLEANUP_SCRIPT_CONTENT=$(cat cleanup.sh)
-
+echo $SERVICENOW_JS
+sed --version
 # Replace placeholders in README.md while keeping the placeholders
-sed -i "/<!-- servicenow.js-start -->/,/<!-- servicenow.js-end -->/c\<!-- servicenow.js-start -->\n\```bash\n$SERVICENOW_JS\n\```\n<!-- servicenow.js-end -->" README.md
+sed -i "/<!-- servicenow.js-start -->/,/<!-- servicenow.js-end -->/c\\
+<!-- servicenow.js-start -->\\
+\`\`\`bash\\
+$SERVICENOW_JS\\
+\`\`\`\\
+<!-- servicenow.js-end -->" README.md
+
+#sed -i "/<!-- servicenow.js-start -->/,/<!-- servicenow.js-end -->/c\<!-- servicenow.js-start -->\n\```bash\n$SERVICENOW_JS\n\```\n<!-- servicenow.js-end -->" README.md
 #sed -i "/<!-- deploy.sh-start -->/,/<!-- deploy.sh-end -->/c\<!-- deploy.sh-start -->\n\```bash\n$DEPLOY_SCRIPT_CONTENT\n\```\n<!-- deploy.sh-end -->" README.md
 #sed -i "/<!-- cleanup.sh-start -->/,/<!-- cleanup.sh-end -->/c\<!-- cleanup.sh-start -->\n\```bash\n$CLEANUP_SCRIPT_CONTENT\n\```\n<!-- cleanup.sh-end -->" README.md
 
