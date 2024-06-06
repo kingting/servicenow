1305d403a152cc07a3960d758b56e7df07188cbe
King Ting <king@ozhut.com>
2024-06-06 18:02:56 +1000

    Using different git to detect change

--- .github/actions/update-readme/update-readme.sh
+++ .github/actions/update-readme/update-readme.sh
@@ -18,17 +18,13 @@ fi
 # Replace placeholders in README.md while keeping the placeholders
 sed -i -e '/servicenow.js-start/,/servicenow.js-end/ {//!d; /servicenow.js-start/r servicenow.js' -e '}' README.md
 
-# Check if there are changes
-if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-  if git diff --exit-code; then
-    echo "No changes detected, skipping commit."
-    echo "no_changes=true" >> $GITHUB_ENV
-  else
-    echo "Changes detected, committing."
-    echo "no_changes=false" >> $GITHUB_ENV
-  fi
+# Check for changes in README.md
+if git diff --quiet README.md; then
+  echo 'No changes detected in README.md.'
+  no_changes=true
 else
-  echo "Not a git repository, skipping commit check."
-  echo "no_changes=true" >> $GITHUB_ENV
+  echo 'Changes detected in README.md.'
+  no_changes=false
 fi
+echo "no_changes=${no_changes}" >> $GITHUB_ENV
 set +x
